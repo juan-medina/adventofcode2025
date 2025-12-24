de5259c80d9d93a517217d0cfe15487e15f93169
Juan Medina <jamedina@gmail.com>
2025-12-24 10:43:39 +0000

    Implement Tachyon beam traversal for Day 7
    Added the Tachyon class to parse, manipulate, and traverse a map for beam splitting logic. Implemented part1 to use this logic, and added comprehensive tests for parsing, setting, stringifying, and traversing the map.

--- src/days/day07.ts
+++ src/days/day07.ts
@@ -3,11 +3,98 @@
 
 import {Day} from "./day";
 
+export enum Space {
+	Start = "S",
+	Empty = ".",
+	Splitter = "^",
+	Beam = "|",
+}
+
+export interface Size {
+	width: number;
+	height: number;
+}
+
+export interface Position {
+	x: number;
+	y: number;
+}
+
+export class Tachyon {
+	map: string[] = [];
+	size: Size = {width: 0, height: 0}
+	start: Position = {x: 0, y: 0};
+
+	constructor(width = 0, height = 0) {
+		this.size = {width, height};
+		for (let i = 1; i < this.size.width; i++) {
+			this.map.push("".padStart(this.size.width, Space.Empty));
+		}
+	}
+
+	static from(input: string): Tachyon {
+		const tachyon = new Tachyon();
+		tachyon.map = input.split("\n");
+		tachyon.size.height = tachyon.map.length;
+		tachyon.size.width = tachyon.map[0].length;
+		for (let y = 0; y < tachyon.size.height; y++) {
+			for (let x = 0; x < tachyon.size.width; x++) {
+				if (tachyon.map[y][x] === Space.Start) {
+					tachyon.start = {x, y};
+				}
+			}
+		}
+		return tachyon;
+	}
+
+	at(x: number, y: number): Space {
+		return this.map[y][x] as Space;
+	}
+
+	set(x: number, y: number, value: Space) {
+		const row = this.map[y];
+		this.map[y] = row.substring(0, x) + value + row.substring(x + 1);
+	}
+
+	string(): string {
+		return this.map.join("\n");
+	}
+
+	shoot(): number {
+		return this.traverse(this.start.x, this.start.y);
+	}
+
+	traverse(x: number, y: number): number {
+		let splits = 0;
+		if (y + 1 < this.size.height) { // can we go down?
+			if (this.at(x, y + 1) === Space.Empty) {
+				this.set(x, y + 1, Space.Beam);
+				return this.traverse(x, y + 1);
+			} else {
+				if (this.at(x, y + 1) === Space.Splitter) {
+					splits += 1;
+					// check if left is possible
+					if (x - 1 >= 0 && this.at(x - 1, y + 1) === Space.Empty) {
+						this.set(x - 1, y + 1, Space.Beam);
+						splits += this.traverse(x - 1, y + 1);
+					}
+					// check if right is possible
+					if (x + 1 < this.size.width && this.at(x + 1, y + 1) === Space.Empty) {
+						this.set(x + 1, y + 1, Space.Beam);
+						splits += this.traverse(x + 1, y + 1);
+					}
+				}
+			}
+		}
+
+		return splits
+	}
+}
 
 export class Day07 extends Day {
 
 	part1(input: string): string {
-		return "";
+		return Tachyon.from(input).shoot().toString();
 	}
 
 	part2(input: string): string {
--- test/days/day07.test.ts
+++ test/days/day07.test.ts
@@ -3,15 +3,101 @@
 
 import {describe, expect, it} from 'vitest';
 import dedent from 'ts-dedent';
-import {day07} from '../../src/days/day07';
+import {day07, Tachyon, Space} from '../../src/days/day07';
 
 describe('Day 7', () => {
 
+	it('can parse tachyon map', () => {
+		const data = dedent`
+			..S..
+			..^..
+			.^...
+		`;
+
+		const tachyon = Tachyon.from(data);
+		expect(tachyon.size).toEqual({width: 5, height: 3});
+
+		expect(tachyon.map.length).toBe(3);
+		expect(tachyon.map[0]).toBe("..S..");
+		expect(tachyon.map[1]).toBe("..^..");
+		expect(tachyon.map[2]).toBe(".^...");
+
+		expect(tachyon.start).toEqual({x: 2, y: 0});
+
+		expect(tachyon.at(2, 0)).toBe(Space.Start);
+		expect(tachyon.at(1, 1)).toBe(Space.Empty);
+		expect(tachyon.at(2, 1)).toBe(Space.Splitter);
+	});
+
+	it(`can set tachyon map`, () => {
+		let tachyon = new Tachyon(5, 3);
+		expect(tachyon.map[0]).toBe(".....");
+		expect(tachyon.map[1]).toBe(".....");
+		expect(tachyon.map[1]).toBe(".....");
+
+		tachyon.set(2, 0, Space.Start);
+		tachyon.set(2, 1, Space.Splitter);
+		tachyon.set(4, 2, Space.Beam);
+
+		expect(tachyon.at(2, 0)).toBe(Space.Start);
+		expect(tachyon.at(2, 1)).toBe(Space.Splitter);
+		expect(tachyon.at(4, 2)).toBe(Space.Beam);
+
+		expect(tachyon.map[0]).toBe("..S..");
+		expect(tachyon.map[1]).toBe("..^..");
+		expect(tachyon.map[2]).toBe("....|");
+	})
+
+	it(`expect tachyon string to match map`, () => {
+		const data = dedent`
+			..S..
+			..^..
+			.^...
+		`;
+
+		const tachyon = Tachyon.from(data);
+		expect(tachyon.string()).toBe(data);
+	});
+
+	it(`shoot tachyon beam`, () => {
+		const data = dedent`
+			..S..
+			..^..
+			.^...
+		`;
+
+		const tachyon = Tachyon.from(data);
+		const splits = tachyon.shoot();
+		expect(splits).toBe(2);
+
+		expect(tachyon.string()).toBe(dedent`
+			..S..
+			.|^|.
+			|^||.
+		`);
+	});
+
 	const input = dedent`
+		.......S.......
+		...............
+		.......^.......
+		...............
+		......^.^......
+		...............
+		.....^.^.^.....
+		...............
+		....^.^...^....
+		...............
+		...^.^...^.^...
+		...............
+		..^...^.....^..
+		...............
+		.^.^.^.^.^...^.
+		...............
     `;
 
 	it('solves part 1 example', () => {
-		expect(day07.part1(input)).toBe("");
+		expect(day07.part1(input)).toBe("21");
 	});
 
 });
