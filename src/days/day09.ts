47916e113300cf5be8f8497d1d9a05b2a2671e10
Juan Medina <jamedina@gmail.com>
2025-12-26 11:00:36 +0000

    Implement rectangle area logic for Day 9
    Added Position, Tile, and MovieTheater classes to parse input and compute rectangle areas between tiles. Updated part1 to return the largest rectangle area. Added comprehensive tests for parsing, area calculation, and rectangle generation.

--- src/days/day09.ts
+++ src/days/day09.ts
@@ -3,10 +3,80 @@
 
 import {Day} from "./day";
 
+export class Position {
+	col: number
+	row: number
+
+	constructor(col: number, row: number) {
+		this.col = col;
+		this.row = row;
+	}
+}
+
+export enum Colors {
+	Red = '#',
+}
+
+export class Tile extends Position {
+	color: Colors;
+
+	constructor(col: number, row: number) {
+		super(col, row);
+		this.color = Colors.Red;
+	}
+
+	static from(data: string): Tile {
+		const [x, y] = data.split(',').map(Number);
+		return new Tile(x, y);
+	}
+
+	area(other: Tile): number {
+		const dx = Math.abs(this.col - other.col) + 1;
+		const dy = Math.abs(this.row - other.row) + 1;
+		return dx * dy;
+	}
+}
+
+
+interface Rectangle {
+	from: Position;
+	to: Position;
+	area: number;
+}
+
+export class MovieTheater {
+	tiles: Tile[];
+
+	constructor(tiles: Tile[]) {
+		this.tiles = tiles;
+	}
+
+	static from(lines: string): MovieTheater {
+		const tiles = lines.split('\n').map(line => Tile.from(line));
+		return new MovieTheater(tiles);
+	}
+
+
+	rectangles(): Rectangle[] {
+		const edges: Rectangle[] = [];
+		for (let i = 0; i < this.tiles.length; i++) {
+			for (let j = i + 1; j < this.tiles.length; j++) {
+				const from = this.tiles[i];
+				const to = this.tiles[j];
+				const area = from.area(to);
+				edges.push({from, to, area});
+			}
+		}
+		return edges.sort((a, b) => b.area - a.area);
+	}
+}
+
 export class Day09 extends Day {
 
 	part1(input: string): string {
-		return "";
+		const theater = MovieTheater.from(input);
+		const rectangles = theater.rectangles();
+		return rectangles[0].area.toString();
 	}
 
 
--- test/days/day09.test.ts
+++ test/days/day09.test.ts
@@ -3,15 +3,89 @@
 
 import {describe, expect, it} from 'vitest';
 import dedent from 'ts-dedent';
-import {day09} from '../../src/days/day09';
+import {Colors, day09, MovieTheater, Position, Tile} from '../../src/days/day09';
 
 describe('Day 9', () => {
 
+	it('tile from string', () => {
+		const tile = Tile.from("1,-2");
+		expect(tile).toStrictEqual(new Tile(1, -2));
+		expect(tile.col).toBe(1);
+		expect(tile.row).toBe(-2);
+		expect(tile.color).toBe(Colors.Red);
+	});
+
+	it('tile area calculation', () => {
+		const tile1 = new Tile(1, 2);
+		const tile2 = new Tile(4, 6);
+		expect(tile1.area(tile2)).toBe(20);
+
+		const h1 = new Tile(0, 0);
+		const h2 = new Tile(5, 0);
+		expect(h1.area(h2)).toBe(6);
+
+		const v1 = new Tile(0, 0);
+		const v2 = new Tile(0, 4);
+		expect(v1.area(v2)).toBe(5);
+	});
+
+	it('MovieTheater lines parsing', () => {
+		const input = dedent`
+			1,2
+			-4,0
+			-6,-7
+		`;
+		const tiles = MovieTheater.from(input);
+		expect(tiles.tiles).toHaveLength(3);
+		expect(tiles.tiles[0]).toStrictEqual(new Tile(1, 2));
+		expect(tiles.tiles[1]).toStrictEqual(new Tile(-4, 0));
+		expect(tiles.tiles[2]).toStrictEqual(new Tile(-6, -7));
+	});
+
+	it('MovieTheater rectangles calculation', () => {
+		const input = dedent`
+			1,1
+			1,4
+			4,1
+		`;
+		const theater = MovieTheater.from(input);
+		const rectangles = theater.rectangles();
+		expect(rectangles).toHaveLength(3);
+
+		expect(rectangles[0]).toEqual({
+			from: new Tile(1, 4),
+			to: new Tile(4, 1),
+			area: 16
+		});
+
+		expect(rectangles[1]).toEqual({
+			from: new Tile(1, 1),
+			to: new Tile(1, 4),
+			area: 4
+		});
+
+		expect(rectangles[2]).toEqual({
+			from: new Tile(1, 1),
+			to: new Tile(4, 1),
+			area: 4
+		});
+
+	});
+
+
 	const input = dedent`
+		7,1
+		11,1
+		11,7
+		9,7
+		9,5
+		2,5
+		2,3
+		7,3
     `;
 
 	it('solves part 1 example', () => {
-		expect(day09.part1(input)).toBe("");
+		expect(day09.part1(input)).toBe("50");
 	});
 
 	it('solves part 2 example', () => {
